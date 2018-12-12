f814ea1c9f362d9ca7297d9f1e61e3faa498fdf7
Mareks Kalējs <M.Kalejs@outlook.com>
2018-12-12 18:10:40 +0100

    Initial commit

---
+++ fsharpperformance/CalcLogic.fs
@@ -0,0 +1,37 @@
+﻿module CalcLogic
+open System
+open System.Diagnostics
+
+let mutable size = 0UL :UInt64
+let mutable msList = []
+
+//Stopwatches
+let stopWatch = new Stopwatch() 
+let ResetStopWatch() = stopWatch.Reset(); stopWatch.Start()
+
+// define the square function
+let square x :int64 = x * x
+
+//Sums squares in recursion
+let rec sumOfSquares(n :int64) =
+    if not (n = 0L) then
+        size <- (size + uint64(square n))
+        sumOfSquares (n - 1L)
+
+//Runs logic x times in recursion
+let rec startCalculation(value, times) =
+    if not (times = 0)then  
+        size <- 0UL
+        ResetStopWatch()
+        sumOfSquares(value)
+        let ms = int(stopWatch.ElapsedMilliseconds)
+        msList <- List.append msList [ms]
+        startCalculation(value, (times - 1))
+
+//Calculates average time
+let averageTime(z) = 
+    let mutable averageMs = 0 :int
+    List.iter( fun ms -> averageMs <- (averageMs + ms / msList.Length) )msList
+    printfn "Average elapsed time in ms: %i" averageMs
+    averageMs
+
---
+++ fsharpperformance/Program.fs
@@ -0,0 +1,19 @@
+﻿open System
+open CalcLogic
+
+
+[<EntryPoint>]
+let main argv = 
+    while true do 
+        printfn "Enter a number to calculate sum of squares"
+        let number = Console.ReadLine() |> int64
+        printfn "Enter how many times to run it"
+        let times = Console.ReadLine() |> int
+        msList <- []
+        startCalculation(number, times)
+        printfn "Value: %O" size 
+        let time = averageTime(-1)
+        if (time = -1)then
+            printf"Something went wrong calculating average time"
+
+    0 // return an integer exit code
